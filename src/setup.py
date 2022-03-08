d83db30c2d74dfdbf03cf80c4d1a6b18e32e749a
RomainJMend <lhotteromain@gmail.com>
2022-03-08 20:25:11 +0100

    First commit - resources (gym environment, structure globalement). V4.

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neuron_poker",
+    version="0.0.0",
+    description="",
+    author="Timothee RIO, Romain LHOTTE",
+    packages=["neuron_poker"],
+    package_dir={
+        "neuron_poker": "neuron_poker"}
+)
